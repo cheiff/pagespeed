5c922b1dfff0b312905296b245f51740cb1d181f
root <root@mx.uk.ocserver.net>
2014-06-26 22:16:42 +0100

    Removed directory after install

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+PACKAGE="mod-pagespeed-stable_current_amd64.deb"
 mkdir temp
 cd temp
-wget https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_amd64.deb
-sudo dpkg -i mod-pagespeed-stable_current_amd64.deb
+wget https://dl-ssl.google.com/dl/linux/direct/$PACKAGE
+sudo dpkg -i $PACKAGE
 sudo apt-get -f install -y
 sudo /etc/init.d/apache2 restart
+cd ..
+rm temp/ -rf
+
