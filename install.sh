007f2db7ba7acc6593b0ff7069baf81a4a92d45d
Javi <root@mx.mad.ocserver.net>
2014-07-01 23:48:11 +0200

    Fixed temp directory

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ wget https://dl-ssl.google.com/dl/linux/direct/$PACKAGE
 sudo dpkg -i $PACKAGE
 sudo apt-get -f install -y
 sudo /etc/init.d/apache2 restart
+chown 999.999 /var/cache/mod_pagespeed
 cd ..
 rm temp/ -rf
 
